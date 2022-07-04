83b81a65c80458dc5b50df8bf9cf6dcbba94f9e1
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-07-04 20:56:48 +0300

    ex1-7

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -86,13 +86,15 @@ $isCurrentHour = getdate();
         <!-- /nav -->
         <!-- breadcrumbs -->
         <? if ($APPLICATION->GetCurPage() != "/") : ?>
-            <div class="breadcrumbs-box">
-                <div class="inner-wrap">
-                    <a href="">Главная</a>
-                    <a href="">Мебель</a>
-                    <span>Выставки и события</span>
-                </div>
-            </div>
+            <? $APPLICATION->IncludeComponent(
+                "bitrix:breadcrumb",
+                "breadcrump",
+                Array(
+                    "PATH" => "",
+                    "SITE_ID" => "s1",
+                    "START_FROM" => "0"
+                )
+            ); ?>
         <? endif; ?>
         <!-- /breadcrumbs -->
         <!-- page -->
