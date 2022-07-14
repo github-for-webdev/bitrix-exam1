99fe8f81641e9283c9e8c0cb91d08305ad7abb7c
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-07-14 21:09:34 +0300

    ex1-8

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -129,14 +129,25 @@ IncludeTemplateLangFile(__FILE__);
             <nav class="main-menu">
                 <div class="item">
                     <div class="title-block">О магазине</div>
-                    <ul>
-                        <li><a href="">Отзывы</a>
-                        </li>
-                        <li><a href="">Руководство </a>
-                        </li>
-                        <li><a href="">История</a>
-                        </li>
-                    </ul>
+                    <? $APPLICATION->IncludeComponent(
+                        "bitrix:menu", 
+                        "bottom_exam1", 
+                        array(
+                            "ALLOW_MULTI_SELECT" => "N",
+                            "CHILD_MENU_TYPE" => "",
+                            "DELAY" => "N",
+                            "MAX_LEVEL" => "1",
+                            "MENU_CACHE_GET_VARS" => array(
+                            ),
+                            "MENU_CACHE_TIME" => "3600",
+                            "MENU_CACHE_TYPE" => "A",
+                            "MENU_CACHE_USE_GROUPS" => "Y",
+                            "ROOT_MENU_TYPE" => "bottom_exam1",
+                            "USE_EXT" => "N",
+                            "COMPONENT_TEMPLATE" => "bottom_exam1"
+                        ),
+                        false
+                    ); ?>
                 </div>
                 <div class="item">
                     <div class="title-block">Каталог товаров</div>
