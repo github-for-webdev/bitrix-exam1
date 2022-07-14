838727d24597bf2d722844986e1c077346400a05
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-07-14 21:16:15 +0300

    ex1-9

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -179,7 +179,16 @@ IncludeTemplateLangFile(__FILE__);
                 <div class="title-block"><?= GetMessage("CONTACT_INFO"); ?></div>
                 <div class="loc-block">
                     <div class="address">ул. Летняя, стр.12, офис 512</div>
-                    <div class="phone"><a href="tel:84952128506">8 (495) 212-85-06</a>
+                    <? $APPLICATION->IncludeComponent(
+                        "bitrix:main.include",
+                        "",
+                        Array(
+                            "AREA_FILE_SHOW" => "file",
+                            "AREA_FILE_SUFFIX" => "inc",
+                            "EDIT_TEMPLATE" => "",
+                            "PATH" => "/include/phone.php"
+                        )
+                    ); ?>
                     </div>
                 </div>
                 <div class="main-soc-block">
