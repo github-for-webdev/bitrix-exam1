a63d80f45a9f1e49be75eb11d01d76aa36f4f835
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-07-14 21:45:40 +0300

    ex1-10

---
+++ local/templates/exam1/page_templates/.content.php
@@ -0,0 +1,6 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+$TEMPLATE["inner.php"] = array(
+    "name" => "Новый шаблон",
+    "sort" => 1
+);
---
+++ template-inner/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "Новый шаблон";
+$arDirProperties = Array(
+
+);
+?>
